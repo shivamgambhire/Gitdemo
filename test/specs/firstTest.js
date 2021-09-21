697ee8d1b9ecf3c0b063b7cfb8852ac909f76440
GAMBHIRE <shivam.gambhire@hach.com>
2021-09-21 15:59:00 +0530

    this is wdio project practice

---
+++ test/specs/firstTest.js
@@ -0,0 +1,40 @@
+describe("this is ecommerce test suite",()=>{
+
+    //xit means this blok of it() is skipped by webdriverio
+    xit("Login check page navigation", ()=>{
+
+        //navigate to url
+        browser.url("https://www.rahulshettyacademy.com/loginpagePractise/");
+
+       // console.log(browser.getTitle());
+       // browser.pause(3000);
+       // expect(browser).toHaveTitle("Google");
+
+       //identifing the locators
+        const user = $("#username")
+        const pass = $("//*[@type='password']")
+
+        //giving input to username and password
+        user.setValue("shivamgambhire")
+        pass.setValue("123456789")
+
+        //sign in button locator
+        const sendbtn = $("#signInBtn")
+        sendbtn.click()
+        
+        //pause the execution
+        browser.pause(2000)
+   
+        //geeting alert message from 
+        const msg = $(".alert-danger")
+        const alt_Msg = msg.getText()
+        
+        console.log(alt_Msg)
+
+        browser.waitUntil(()=> sendbtn.getAttribute("value") === 'Sign In', {
+            timeout: 3000, timeoutMsg: 'the alert message is not found'
+        })
+              
+        console.log($(".alert-danger").getText()) + "this is after execution"
+    });
+});
